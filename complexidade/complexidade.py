1052fe6287ddb90f8b2cde74fede0685ffe6d902
Luiz Fernando Oliveira Maciel <lfnando.nando@gmail.com>
2022-12-11 23:33:49 -0300

    Add files via upload

--- complexidade/complexidade.py
+++ complexidade/complexidade.py
@@ -2,7 +2,10 @@ import random
 import copy
 import time
 import sys
-import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
 sys.setrecursionlimit(100000)
 start = time.time()
@@ -78,7 +81,7 @@ def comp(l1, l2):
 
 # Encontra um conjunto de caminhos disjuntos que possui o número
 # máximo de caminhos disjuntos possíveis para o grafo
-def acharCaminhos(origem, destino, grafo, n, cam_dis):
+def acharCaminhos(origem, destino, grafo, n):
     visitado = [False] * len(grafo)
     caminho = []
     caminhos = []
@@ -105,13 +108,7 @@ def acharCaminhos(origem, destino, grafo, n, cam_dis):
                 visitados.extend(bDuplas)
 
         if len(disjuntos) == n:
-            with open(cam_dis, 'w') as f:
-                for i in disjuntos:
-                    f.write(str(i) + "\n")
             return disjuntos
-
-
-    print("acharCaminhos num: ", len(disjuntos))
     
     return []
 
@@ -121,7 +118,7 @@ def acharCaminhos(origem, destino, grafo, n, cam_dis):
 # o método acharCaminho um subgrafo com apenas as arestas que possuem algum 
 # fluxo passando por elas, o que reduz a complexidade do código em um grau 
 # de magnitude
-def caminhosDisjuntos(origem, destino, g, cam_dis):
+def caminhosDisjuntos(origem, destino, g):
     grafo = copy.deepcopy(g)
     pai = [-1] * len(grafo)
     fluxo_maximo = 0
@@ -143,7 +140,7 @@ def caminhosDisjuntos(origem, destino, g, cam_dis):
 
     dif = difMatriz(g, grafo)
 
-    acharCaminhos(origem, destino, dif, fluxo_maximo, cam_dis)
+    acharCaminhos(origem, destino, dif, fluxo_maximo)
     
     return fluxo_maximo
         
@@ -193,104 +190,44 @@ def gerarGrafoCompleto(n):
 # NÚMERO DE VÉRTICES
 v = 10
 
-n = len(sys.argv)
-if (n > 0):
-    v = sys.argv[0]
-    # if (n > 1):
-
-# Parsing dos argumentos inline
-# UTILIZAÇÃO
-
-# -prob [float]: Probabilidade que dois vértices tenham uma aresta em comum 
-# (Utilizado na geração de grafos aleatórios)
-# Caso não seja utilizado a probabilidade será 0.3
-# Exemplo: -prob 0.1
-
-# -arq [string]: Diretório de um arquivo txt com a matriz de adjacência de um grafo
-# Exemplo: -arq "./grafos cíclicos/50.txt"
-
-# -ver [int]: Número de vértices a ser usado na geração de grafo
-# Caso não seja utilizado o número de vértices será 10
-# Exemplo: -ver 100
-
-# -ori [int]: Vértice de origem
-# Caso não seja utilizado, o vértice de origem será 0
-# Exemplo: -ori 1
-
-# -des [int]: Vértice de destino
-# Caso não seja utilizado, o vértice de destino será n - 1
-# Exemplo: -des 9
-
-# -cam [string]: Diretório para salvar os caminhos disjuntos encontrados
-# Caso não seja utilizado, os caminhos serão salvos em "./disjuntos.txt"
-# Exemplo: -cam "./out/caminhos_disjuntos.txt"
-
-# -w [string]: Diretório para salvar o grafo gerado
-# Caso não seja utilizado o grafo gerado não será salvo
-# Exemplo: -w "./grafos simples/50.txt"
-
-# -ciclo: Gera um grafo cíclico
-
-# -completo: Gera um grafo completo
-parser = argparse.ArgumentParser()
-parser.add_argument("-prob", "--probabilidade", help="Probabilidade de dois vértices quaisquer serem conectados por uma aresta")
-parser.add_argument("-arq", "--arquivo", help="Diretório para um grafo")
-parser.add_argument("-ver", "--vertices", help="Número de vértices")
-parser.add_argument("-ori", "--origem", help="Vértice de origem")
-parser.add_argument("-des", "--destino", help="Vértice de destino")
-parser.add_argument("-cam", "--cam_dir", help="Diretório para salvar os caminhos disjuntos encontrados")
-parser.add_argument("-w", "--salvar_dest", help="Diretório para salvar")
-parser.add_argument("-ciclo", action='store', nargs='*')
-parser.add_argument("-completo", action='store', nargs='*')
-args = parser.parse_args()
-
-v = 10
-p = 0.3
-grafo = []
-cam_dir = "./disjuntos.txt"
 origem = 0
 destino = v - 1
 
-if (args.vertices != None):
-    v = int(args.vertices)
+num_vertices = []
+tempo_exec = []
+for i in range(10, 1000, 10):
+    num_vertices.append(i)
+    startTime = time.time()
+    grafo = grafoAleatorio(i, 0.5)
+    caminhosDisjuntos(origem, destino, grafo)
+    endTime = time.time()
+    tempo_exec.append((endTime - startTime) * 10**3) # ms
 
-if (args.probabilidade != None):
-    p = float(args.probabilidade)
+with open("vertices.txt", "w") as file:
+    for i in num_vertices:
+        file.write(str(i) + '\n')
 
-if (args.ciclo != None or args.completo != None):
-    if (args.ciclo != None):
-        grafo = gerarGrafoCiclico(v)
-    else:
-        grafo = gerarGrafoCompleto(v)
-elif (args.arquivo != None):
-    grafo = getGrafo(args.arquivo)
-    v = len(grafo)
-else:
-    grafo = grafoAleatorio(v, p)
+with open("tempo_exec.txt", "w") as file:
+    for i in tempo_exec:
+        file.write(str(i) + '\n')
 
-if (args.cam_dir != None):
-    cam_dir = args.cam_dir
+plt.scatter(num_vertices, tempo_exec)
+plt.savefig("complexidade.png")
 
-if (args.salvar_dest != None):
-    salvarGrafo(grafo, args.salvar_dest)
+x = np.array(num_vertices).reshape((-1, 1))
+y = np.array(tempo_exec)
 
-if (args.origem != None):
-    origem = int(args.origem)
-    if origem < 0:
-        print("Erro: vértice de origem inválido. Use um vértice com índice maior ou igual a 0")
-        origem = 0
+transformer = PolynomialFeatures(degree=2, include_bias=False)
+transformer.fit(x)
 
-if (args.destino != None):
-    destino = int(args.destino)
-    if destino > v - 1:
-        print("Erro: vértice de destino inválido. Use um vértice com valor menor ou igual a ", v-1)
-        destino = v - 1
+x_ = transformer.transform(x)
+model = LinearRegression().fit(x_, y)
 
-numCaminhosDisjuntos = caminhosDisjuntos(origem, destino, grafo, cam_dir)
-print("Número de caminhos: ", numCaminhosDisjuntos)
+r_sq = model.score(x_, y)
+print("R^2: ", r_sq)
+
+print("Coeficientes: ", model.coef_)
+
 
 end = time.time()
 print("Tempo de execucação: ", (end - start) * 10**3, "ms")
-with open(cam_dir, "a") as f:
-    f.write("Número de caminhos: " + str(numCaminhosDisjuntos) + "\n")
-    f.write("Tempo de execucação: " + str((end - start) * 10**3) +  "ms\n")
